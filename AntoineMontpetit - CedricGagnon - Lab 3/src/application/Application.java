a3c5fbef216f02d582858f704939c4540f518fce
Inlinkcraft <47537121+Inlinkcraft@users.noreply.github.com>
2020-11-05 11:33:07 -0500

    Projet de Base
    Ceci devrais être le project de base

---
+++ AntoineMontpetit - CedricGagnon - Lab 3/src/application/Application.java
@@ -0,0 +1,118 @@
+package application;
+import java.util.Scanner;
+import dessins_de_formes.DessinGeometrique;
+
+/**
+ * Cette classe permet d'afficher plusieurs dessins géométriques en utilisant 
+ * la classe DessinGeometrique.
+ *
+ * @author Vos noms
+ * @version Automne 2020
+ */
+public class Application
+{
+	/**
+	 * Demande à l'utilisateur d'entrer dans la console les dimensions du cadre.
+	 * La méthode demande à l'utilisateur (dans la console) s'il veut saisir les
+	 * dimensions d'un nouveau cadre. Si l'utilisateur répond 'o' pour oui, tant
+	 * que les dimensions ne sont pas valides, la méthode redemande une
+	 * nouvelle saisie du cadre. Lorsque le cadre est saisi correctement, cette
+	 * méthode retourne un nouvel objet DessinGeometrique créé à partir des
+	 * dimensions saisies. Si l'utilisateur répond 'n' pour non, la méthode
+	 * retourne la valeur "null", à ce moment le programme se termine.
+	 * 
+	 * @return un nouvel objet DessinGeometrique si les valeurs ont été saisies
+	 *         sinon la valeur "null".
+	 */
+	public static DessinGeometrique demanderCadre()
+	{
+		DessinGeometrique retForme = null;
+		int baseSaisie = 0;
+		int hauteurSaisie = 0;
+		Scanner entree = new Scanner(System.in);
+
+		// Veut-on changer le cadre
+		System.out.println("Voulez-vous saisir un autre cadre? o/n");
+
+		// Si l'on change le cadre...
+		if (entree.next().toUpperCase().compareTo("O") == 0)
+		{
+
+			// TODO - À faire - Étape 3
+			System.out.println(
+					"Aucune saisie pour le moment!!! Complétez le code...");
+			retForme = null;
+		}
+
+		return retForme;
+	}
+
+	/**
+	 * Méthode principale pour afficher les dessins
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		DessinGeometrique forme1 = new DessinGeometrique(14, 5);
+		DessinGeometrique forme2 = new DessinGeometrique(16, 5);
+		DessinGeometrique forme3 = new DessinGeometrique(25, 8);
+		DessinGeometrique forme4 = new DessinGeometrique(4, 10);
+
+		// Avec valeurs prédéterminées 
+		System.out
+				.println("On affiche les formes avec les valeurs initiales\n");
+		System.out.println("Ligne horizontale, " + forme1 + "\n");
+		System.out.println(forme1.creerLigneHorizontale() + "\n");
+
+		System.out.println("Ligne verticale, " + forme1 + "\n");
+		System.out.println(forme1.creerLigneVerticale() + "\n");
+
+		System.out.println("Contour carré, " + forme2 + "\n");		
+		System.out.println(forme2.gererContourCarre() + "\n");;
+
+		System.out.println("Contour rectangle, " + forme2 + "\n");		
+		System.out.println(forme2.gererContourRectangle() + "\n");
+
+		System.out.println("Triangle droit, " + forme4 + "\n");		
+		System.out.println(forme4.creerTriangleDroit() + "\n");
+
+		System.out.println("Triangle gauche, " + forme3 + "\n");		
+		System.out.println(forme3.creerTriangleGauche() + "\n");	
+
+		// Difficile
+		System.out.println("Losange, " + forme3 + "\n");
+		System.out.println(forme3.creerLosange() + "\n");
+
+		// BONUS : Difficile à faire, faire à la fin,
+		//System.out.println("Cercle, " + forme3 + "\n");
+		// System.out.println(forme3.creerCercle() + "\n");
+
+		// Avec valeur demandée et validée
+		System.out.println(
+				"On affiche les formes dans les cadres fournis par l'utilisateur\n");
+
+		while ((forme1 = demanderCadre()) != null)
+		{
+			System.out.println("\n" + forme1 + "\n");			
+			
+			System.out.println(forme1.creerLigneHorizontale() + "\n");				
+			
+			System.out.println(forme1.creerLigneVerticale() + "\n");			
+			
+			System.out.println(forme1.gererContourCarre() + "\n");			
+			
+			System.out.println(forme1.gererContourRectangle() + "\n");
+			
+			System.out.println(forme1.creerTriangleDroit() + "\n");
+						
+			System.out.println(forme1.creerTriangleGauche() + "\n");
+			
+			System.out.println(forme1.creerLosange() + "\n");
+
+			// Mettre ici l'affichage du cercle, si vous l'avez fait...
+		}
+
+		System.out.println("\nMerci d'avoir utilisé ce programme !");
+	}
+}
---
+++ AntoineMontpetit - CedricGagnon - Lab 3/src/dessins_de_formes/DessinGeometrique.java
@@ -0,0 +1,234 @@
+package dessins_de_formes;
+
+/**
+ * Cette classe permet de créer plusieurs dessins géométriques. L'utilisateur
+ * peut définir les dimensions (base et hauteur) du cadre qui contient la forme.
+ *
+ * @author Vos noms
+ * @version Automne 2020
+ */
+public class DessinGeometrique
+{
+	// constantes pour valeurs limites acceptables
+	public final static int BASE_MAX = 50;
+	public final static int HAUTEUR_MAX = 20;
+
+	// constantes pour valeurs par défaut
+	public final static int BASE_DEFAUT = 5;
+	public final static int HAUTEUR_DEFAUT = 5;
+
+	// Propriétés du cadre qui délimite la forme géométrique.
+	// La base correspond au nombre de colonnes nécessaires pour dessiner la forme.
+	// La hauteur correspond au nombre de lignes nécessaires pour dessiner la forme.
+	int base = 0;
+	int hauteur = 0;
+
+	/**
+	 * Constructeur unique
+	 */
+	public DessinGeometrique(int pBase, int pHauteur)
+	{
+		if (validerCadre(pBase, pHauteur))
+		{
+			setCadre(pBase, pHauteur);
+		}
+		else
+		{			
+			setCadre(BASE_DEFAUT, HAUTEUR_DEFAUT);
+			
+			// TODO - À faire - Étape 4
+			// Donner des valeurs aléatoires.
+		}
+	}
+
+	/**
+	 * Permet de modifier la base et la hauteur du cadre
+	 * 
+	 * @param pBase, nouvelle valeur de la base
+	 * @param pHauteur, nouvelle valeur de la hauteur
+	 */
+	private void setCadre(int pBase, int pHauteur)
+	{
+		if (validerCadre(pBase, pHauteur))
+		{
+			base = pBase;
+			hauteur = pHauteur;
+		}
+	}
+
+	/**
+	 * La base de la forme.
+	 *
+	 * @return la base
+	 */
+	public int getBase()
+	{
+		return base;
+	}
+
+	/**
+	 * La hauteur de la forme.
+	 *
+	 * @return la hauteur
+	 */
+	public int getHauteur()
+	{
+		return hauteur;
+	}
+
+	/**
+	 * Retourne le plus petite valeur entre la base et la hauteur. Pensez à
+	 * utiliser la classe Math.
+	 *
+	 * @return la longueur d'un côté.
+	 */
+	private int getPlusPetitCote()
+	{
+		// TODO - À faire - Étape 1
+		return 0;
+	}
+
+	/**
+	 * Évalue si le cadre est valide. Pour que le cadre soit valide, il faut que
+	 * sa hauteur et sa base soient supérieures à 0 et inférieures au bornes
+	 * limites définies par les constantes BASE_MAX et HAUTEUR_MAX.
+	 * 
+	 * @param pBase, base de la forme à valider
+	 * @param pHauteur, hauteur de la forme à valider
+	 * @return vrai si le cadre est valide.
+	 */
+	public static boolean validerCadre(int pBase, int pHauteur)
+	{
+		// TODO - À faire - Étape 1
+		return true;
+	}
+
+	/**
+	 * Créer une ligne horizontale.
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	public String creerLigneHorizontale()
+	{
+		String ligneHorizontale = "";
+		// TODO - À faire - Étape 2a)
+		return ligneHorizontale;
+	}
+	
+	/**
+	 * Créer une ligne verticale.
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	public String creerLigneVerticale()
+	{
+		String ligneVerticale = "";
+		// TODO - À faire - Étape 2a)
+		return ligneVerticale;
+	}
+
+	/**
+	 * Gérer la création du contour d'un carré.
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	public String gererContourCarre()
+	{
+		int cote = getPlusPetitCote();
+		return creerContour(cote, cote);
+	}
+
+	/**
+	 * Gérer la création du contour d'un rectangle.
+	 *
+	 *  @return La chaîne de caractères contenant la forme.
+	 */
+	public String gererContourRectangle()
+	{
+		return creerContour(base, hauteur);
+	}
+
+	/**
+	 * Créer le contour d'un rectangle (dont le carré).
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	private static String creerContour(int pBase, int pHauteur)
+	{
+		String contour = "";
+		// TODO - À faire - Étape 2b)
+		return contour;
+	}
+	
+	/**
+	 * Créer le triangle appuyé à droite
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	public String creerTriangleDroit()
+	{
+		String triangleDroit = "";
+		// TODO - À faire - Étape 2c)
+		return triangleDroit;
+	}
+
+	/**
+	 * Créer le triangle appuyé à gauche
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	public String creerTriangleGauche()
+	{
+		String triangleGauche = "";
+		// TODO - À faire - Étape 2c)
+		return triangleGauche;
+	}	
+
+	/**
+	 * Créer le losange à partir du plus petit côté
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	public String creerLosange()
+	{
+		String losange = "";
+		// TODO - À faire Étape 2d)
+		return losange;	
+	}
+	
+	/**
+	 * Créer le cercle à partir du plus petit côté.
+	 *
+	 * @return La chaîne de caractères contenant la forme.
+	 */
+	public String creerCercle()
+	{
+		String cercle = "";
+		int rayon = getPlusPetitCote() / 2;
+
+		// TODO - À faire - Étape 5
+		return cercle;
+	}
+
+	/**
+	 * Retourne une chaîne qui représente le cadre de la forme.
+	 * 
+	 * @return une chaîne qui représente le cadre de la forme.
+	 */
+	@Override
+	public String toString()
+	{
+		return getHauteur() + " ligne(s) X " + getBase() + " colonne(s).";
+	}
+	
+	/**
+     * Méthode pour faire les tests sur vos méthodes
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+        
+
+    }
+}
